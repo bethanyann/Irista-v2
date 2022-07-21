6b9b14eeb2f349f0e3ef96cc4a531d76b10088fa
bethanyann <bethanyann44@gmail.com>
2022-07-21 18:05:25 -0500

    renaming stuff

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,6 +6,7 @@ import client from './apolloClient';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/authContext';
+
  
 // React application needs access to:
 // Client (ApolloProvider), 
