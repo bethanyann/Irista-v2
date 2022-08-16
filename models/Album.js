9b83d1324b7ce611e818be590f07ad1e706e0154
bethanyann <bethanyann44@gmail.com>
2022-08-15 22:00:52 -0500

    Add photo and album mongoose models

---
+++ models/Album.js
@@ -0,0 +1,10 @@
+const { model, Schema } = require('mongoose');
+
+const albumSchema = new Schema({
+    albumId: String, //this will be the name of the album when created and will not change
+    albumName: String, //this will be mutable
+    albumCreatedAt: Date,
+    albumCreatedBy: String //this will be the users username
+});
+
+module.exports = model('Album', albumSchema);
---
+++ models/Photo.js
@@ -0,0 +1,10 @@
+const { model, Schema } = require('mongoose');
+
+const photoSchema = new Schema({
+    photoId: String,  //this will be the original photo filename (or assetId?) and will not change
+    photoName: String, //this will be the mutable filename 
+    photoLatitude: number,
+    photoLongitude: number,
+    isFavorite: boolean,
+    createdAt: Date,
+})
