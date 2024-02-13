46415178d96eba2e058a7d5904ecf4ce9667d744
bethanyann <bethanyann44@gmail.com>
2024-02-12 22:32:08 -0600

    tree shaking and formating shadowScrollbar

--- client/src/components/shadowScrollbar.js
+++ client/src/components/shadowScrollbar.js
@@ -1,61 +1,53 @@
-import css from 'dom-css';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Scrollbars } from 'react-custom-scrollbars-2';
+// import css from 'dom-css';
+import { Component } from "react";
+import PropTypes from "prop-types";
+import { Scrollbars } from "react-custom-scrollbars-2";
 
 class ShadowScrollbars extends Component {
+  constructor(props, ...rest) {
+    super(props, ...rest);
+    this.state = {
+      scrollTop: 0,
+      scrollHeight: 0,
+      clientHeight: 0,
+    };
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.renderThumb = this.renderThumb.bind(this);
+  }
 
-    constructor(props, ...rest) {
-        super(props, ...rest);
-        this.state = {
-            scrollTop: 0,
-            scrollHeight: 0,
-            clientHeight: 0
-        };
-        this.handleUpdate = this.handleUpdate.bind(this);
-        this.renderThumb = this.renderThumb.bind(this);
-    }
+  //   handleUpdate(values) {}
 
-    handleUpdate(values) {
-      
-    }
+  renderThumb({ style, ...props }) {
+    const thumbStyle = {
+      backgroundColor: `rgb(238,238,238,0.2)`,
+    };
+    return <div style={{ ...style, ...thumbStyle }} {...props} />;
+  }
 
-    renderThumb({ style, ...props }) {
-        const thumbStyle = {
-            backgroundColor: `rgb(238,238,238,0.2)`,
-        };
-        return (
-            <div
-                style={{ ...style, ...thumbStyle }}
-                {...props}/>
-        );
-    }
-
-
-    render() {
-        const { style, ...props } = this.props;
-        const containerStyle = {
-            ...style,
-            position: 'relative'
-        };
-        return (
-            <div style={containerStyle}>
-                <Scrollbars
-                    thumbSize={100}
-                    thumbMinSize={50}
-                    autoHide
-                    ref="scrollbars"
-                    renderThumbVertical={this.renderThumb}
-                    onUpdate={this.handleUpdate}
-                    {...props}/>
-            </div>
-        );
-    }
+  render() {
+    const { style, ...props } = this.props;
+    const containerStyle = {
+      ...style,
+      position: "relative",
+    };
+    return (
+      <div style={containerStyle}>
+        <Scrollbars
+          thumbSize={100}
+          thumbMinSize={50}
+          autoHide
+          ref="scrollbars"
+          renderThumbVertical={this.renderThumb}
+          onUpdate={this.handleUpdate}
+          {...props}
+        />
+      </div>
+    );
+  }
 }
 
 ShadowScrollbars.propTypes = {
-    style: PropTypes.object
+  style: PropTypes.object,
 };
 
 export default ShadowScrollbars;
-
