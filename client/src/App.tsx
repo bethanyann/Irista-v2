455cb0aaeb7cc1c33256624c467ceb346ca911cb
bethanyann <bethanyann44@gmail.com>
2024-02-12 22:31:53 -0600

    tree shaking and formatting app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,80 @@
-import React, { useContext, lazy, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { AuthProvider, AuthContext } from  './context/authContext';
-import { isEmpty } from 'lodash';
-import { GlobalStyle } from './GlobalStyles';
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
+// import { isEmpty } from 'lodash';
+import { GlobalStyle } from "./GlobalStyles";
 //route protection
-import RequireAuth from './context/requireAuth';
+import RequireAuth from "./context/requireAuth";
 //components
-import Register from './pages/Register/register';
-import Login from './pages/Login/login';
+import Register from "./pages/Register/register";
+import Login from "./pages/Login/login";
 // import AlbumGrid from './pages/AlbumGrid/albumGrid';
-import PhotoGrid from './pages/TimelineGrid/timelineGrid';
+import PhotoGrid from "./pages/TimelineGrid/timelineGrid";
 // import UploadPage from './pages/Upload/uploadPage';
-import Landing from './pages/Landing/landing';
-import AlbumPhotos from './pages/AlbumPhotos/albumPhotos';
+import Landing from "./pages/Landing/landing";
+import AlbumPhotos from "./pages/AlbumPhotos/albumPhotos";
 // import SearchResults from './pages/SearchResults/searchResults';
-import { WithNav, WithoutNav } from './components/Navbar/navbarDisplay';
+import { WithNav, WithoutNav } from "./components/Navbar/navbarDisplay";
 
-
-const UploadPage = lazy(() => import('./pages/Upload/uploadPage'));
-const SearchResults = lazy(() => import('./pages/SearchResults/searchResults'));
-const AlbumGrid = lazy(() => import('./pages/AlbumGrid/albumGrid'));
+const UploadPage = lazy(() => import("./pages/Upload/uploadPage"));
+const SearchResults = lazy(() => import("./pages/SearchResults/searchResults"));
+const AlbumGrid = lazy(() => import("./pages/AlbumGrid/albumGrid"));
 
 export default function App() {
   return (
-        <AuthProvider>
-          <Suspense fallback={<h1> Loading route... </h1>}>
-            <Routes>
-              <Route element={<WithNav />} >
-                <Route path='/login' element={<Login/>}/> 
-                <Route path='/register' element={<Register />} /> 
-                <Route path='/photos' element={<RequireAuth><PhotoGrid/></RequireAuth>}/>
-                <Route path='/albums' element={<RequireAuth><AlbumGrid/></RequireAuth>}/>
-                <Route path='/upload' element={<RequireAuth><UploadPage/></RequireAuth>}/>
-                <Route path='/album/:albumName' element={<RequireAuth><AlbumPhotos /></RequireAuth>} />
-                <Route path='/search/:searchTerm' element={<RequireAuth><SearchResults /></RequireAuth>} />
-              </Route>  
-              <Route element={<WithoutNav />}>
-                <Route path='/' element={<Landing />} />
-              </Route>
-            </Routes>
-          <GlobalStyle />
-          </Suspense>
-        </AuthProvider>
+    <AuthProvider>
+      <Suspense fallback={<h1> Loading route... </h1>}>
+        <Routes>
+          <Route element={<WithNav />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/photos"
+              element={
+                <RequireAuth>
+                  <PhotoGrid />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/albums"
+              element={
+                <RequireAuth>
+                  <AlbumGrid />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/upload"
+              element={
+                <RequireAuth>
+                  <UploadPage />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/album/:albumName"
+              element={
+                <RequireAuth>
+                  <AlbumPhotos />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/search/:searchTerm"
+              element={
+                <RequireAuth>
+                  <SearchResults />
+                </RequireAuth>
+              }
+            />
+          </Route>
+          <Route element={<WithoutNav />}>
+            <Route path="/" element={<Landing />} />
+          </Route>
+        </Routes>
+        <GlobalStyle />
+      </Suspense>
+    </AuthProvider>
   );
 }
